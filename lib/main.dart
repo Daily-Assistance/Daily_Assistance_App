8528acc0a7fa69eaa218e54c6c433af11cbf9b99
diadntjr <dntjr0501@gmail.com>
2023-01-02 00:01:35 +0900

    [UPDATE] main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "일상 보조임",
+      title: "일상 보조",
       debugShowCheckedModeBanner: false,
       home: MyPage(),
     );
