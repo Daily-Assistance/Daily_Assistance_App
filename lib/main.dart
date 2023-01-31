a195bc75308aa7925ce98c9e3cac13c1d4d07601
조용제 <whehdrms6810@naver.com>
2023-01-31 22:28:02 +0900

    [ADD 알람

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:daily_assistance/screens/calender.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   initializeDateFormatting().then((value) => runApp(const MyApp()));
@@ -27,11 +28,34 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  
+  get onSelectNotification => null;
+  final notifications = FlutterLocalNotificationsPlugin();
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
     initializeDateFormatting(Localizations.localeOf(context).languageCode);
+  }
+
+  showNotification() async {
+    var androidDetails = AndroidNotificationDetails(
+      '유니크한 알림 채널 ID',
+      '알림종류 설명',
+      priority: Priority.high,
+      importance: Importance.max,
+      color: Color.fromARGB(255, 255, 0, 0),
+    );
+
+    var iosDetails = IOSNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+
+    // 알림 id, 제목, 내용 맘대로 채우기
+    notifications.show(1, '제목1', '내용1',
+        NotificationDetails(android: androidDetails, iOS: iosDetails),
+        payload: '부가정보' // 부가정보
+        );
   }
 
   @override
@@ -52,7 +76,7 @@ class _MyPageState extends State<MyPage> {
           children: [
             ElevatedButton(
                 onPressed: () {
-                  //알림 받는 함수
+                  showNotification();
                 },
                 child: const Text("알림 받기")),
             ElevatedButton(
